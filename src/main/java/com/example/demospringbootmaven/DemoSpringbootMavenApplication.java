a91086c46f00d4063e910e9098c84f65f0f1b4d2
hirakawa.mizuki <hirakawa.mizuki.901@gmail.com>
2020-02-24 22:34:51 +0900

    initial commit

---
+++ src/main/java/com/example/demospringbootmaven/DemoSpringbootMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demospringbootmaven/DemoSpringbootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
